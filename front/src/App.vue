f5296556e56d16c1cdfc1cc7923213a53e87e235
Karamelooo <hugo.petitleneuder@gmail.com>
2025-10-14 18:31:40 +0200

    feat: add icon support on Button component

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,8 +1,12 @@
 <script lang="ts" setup>
 import Button from './components/Button.vue';
-</script>
+import { useToast } from './composables/useToast';
+import ToastContainer from './components/ToastContainer.vue'
 
+const toast = useToast();
+</script>
 <template>
+    <ToastContainer />
     <div id="root">
         <h1>UI Kit — © IZZZI 2025.</h1>
 
@@ -22,6 +26,17 @@ import Button from './components/Button.vue';
             <Button size="small" variant="neutral" :disabled="true">Neutre — désactivé</Button>
             <Button size="small" variant="plain">Sans fond</Button>
             <Button size="small" variant="plain" :disabled="true">Sans fond — désactivé</Button>
+        </div>
+        <h3>Toasts</h3>
+        <div class="row">
+            <Button @click="toast.success('Les étudiants ont bien été relancés.')">Success</button><br>
+            <Button  @click="toast.positive('Alerte', 'Ricotta Chicago Aussie extra pie. Ranch parmesan anchovies sautéed lovers red Chicago stuffed.')">Positive</button><br>
+            <Button  @click="toast.negative('Alerte', 'Ricotta Chicago Aussie extra pie. Ranch parmesan anchovies sautéed lovers red Chicago stuffed.')">Negative</button><br>
+            <Button  @click="toast.info('Plan actuel.')">Info</button><br>
+            <Button  @click="toast.warning('Période d’essai terminée :\n changez de plan pour débloquer les retours illimités.')">Warning</button><br>
+            <Button  @click="toast.infoWarning('Les étudiants ont bien été relancés.')">InfoWarning</button><br>
+            <Button  @click="toast.lock('Les étudiants ont bien été relancés.')">Lock</button><br>
+            <Button  @click="toast.lockWarning('Les étudiants ont bien été relancés.')">LockWarning</button><br>
         </div>
     </div>
 </template>
