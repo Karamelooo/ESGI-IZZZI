b31396d0def782c739f63052e411e3bdfd2f6927
leo P <Theoasis953@gmail.com>
2025-10-14 12:42:27 +0200

    fix: :green_heart: fix vite config for prod host

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-  },
+export default defineConfig(( { mode } ) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [vue()],
+    server: {
+      host: true,
+      port: 5173,
+      strictPort: true,
+      allowedHosts: env.VITE_ALLOWED_HOSTS.split(','),
+    },
+  }
 })
