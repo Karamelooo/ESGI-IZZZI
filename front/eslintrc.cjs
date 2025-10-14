74b3eacc0eb3bd27932a2d42ede46d178472ad97
leo P <Theoasis953@gmail.com>
2025-10-14 11:30:11 +0200

    feat: :sparkles: Init project

---
+++ front/eslintrc.cjs
@@ -0,0 +1,20 @@
+// .eslintrc.cjs
+module.exports = {
+  root: true,
+  env: { browser: true, es2022: true },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'prettier',
+  ],
+  rules: {
+    // Add your rules here
+  },
+}
