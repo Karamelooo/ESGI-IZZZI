7f84f8447830ea0768c6ec7572201f02232cb369
Sohaib Mohammad <sohaib.mohammad745@gmail.com>
2025-10-16 17:49:33 +0200

    feat: move UI kit at /ui-kit

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,7 +1,12 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
 
-const routes: Array<RouteRecordRaw> = [{ path: '/', name: 'home', component: Home }];
+import Home from '../views/Home.vue';
+import UIKit from '../views/UIKit.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  { path: '/', name: 'home', component: Home },
+  { path: '/ui-kit', name: 'ui-kit', component: UIKit },
+];
 
 const router = createRouter({
   history: createWebHistory(),
