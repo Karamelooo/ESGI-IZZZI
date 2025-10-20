0401d617cc459be491c66f011eccada8a9762ecd
Karamelooo <hugo.petitleneuder@gmail.com>
2025-10-21 00:26:07 +0200

    feat: add signup and signin view in login page

---
+++ front/src/components/base/Container.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container"><slot></slot></div>
+</template>
+
+<style>
+.container {
+  border: 1px solid var(--gray-15);
+  border-radius: 8px;
+  padding: 1rem;
+  padding-bottom: 4rem;
+  background-color: var(--white);
+  margin: 0 1rem;
+}
+</style>
--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
 
 import Home from '@views/public/Home.vue';
+import Login from '@views/public/Login.vue';
 import UIKit from '@views/utils/UIKit.vue';
 import PageComponents from '@views/utils/PageComponents.vue';
 
 const routes: Array<RouteRecordRaw> = [
   { path: '/', name: 'home', component: Home },
+  { path: '/login', name: 'login', component: Login },
   { path: '/ui-kit', name: 'ui-kit', component: UIKit },
   { path: '/page-components', name: 'page-components', component: PageComponents },
 ];
