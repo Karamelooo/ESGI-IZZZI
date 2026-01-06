4aac9fe6993c839e1a1a9ca24f0fbb7329aceea5
Karamelooo <hugo.petitleneuder@gmail.com>
2026-01-06 17:10:31 +0100

    feat:csv

--- front/src/api/subjects.ts
+++ front/src/api/subjects.ts
@@ -14,3 +14,9 @@ export async function createSubject(payload: CreateSubjectPayload) {
   const response = await api.post('/subjects', payload);
   return response.data;
 }
+
+export async function createSubjectsBulk(payload: CreateSubjectPayload[]) {
+  const api = useTypedApi();
+  const response = await api.post('/subjects/bulk', payload);
+  return response.data;
+}
